2d9f35fb1b0b8c3c723e0b40f3142380877e0d5d
Dong Ying <dong.ying@rea-group.com>
2014-07-23 01:12:33 +0800

    Change the solution to a simpler one

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,3 +1,5 @@
+require('fizz_buzz')
+
 describe FizzBuzz do
   it "should return fizz if the number is dividable by 3" do
     expect(FizzBuzz.new.getResult(9)).to eq('fizz')
@@ -9,5 +11,9 @@ describe FizzBuzz do
 
   it "should return fizzbuzz if the number is dividable by 15" do
     expect(FizzBuzz.new.getResult(45)).to eq('fizzbuzz')
+  end
+
+  it "should return the number itself if the number is not dividable by 3, 5 or 15" do
+    expect(FizzBuzz.new.getResult(77)).to eq(77)
   end
 end
--- src/fizz_buzz.rb
+++ src/fizz_buzz.rb
@@ -1,8 +1,20 @@
 class FizzBuzz
   def getResult number
-    fizz_buzz_match = {3 => "fizz", 5 => "buzz"}
-    fizz_buzz_match.keys.inject("") do |result, key|
-     (number % key == 0)? (result + fizz_buzz_match[key]): result
+    #fizz_buzz_match = {3 => "fizz", 5 => "buzz"}
+    #fizz_buzz_match.keys.inject(number) do |result, key|
+    #  (number % key == 0) ? (result + fizz_buzz_match[key]) : result
+    #end
+
+
+    case true
+      when number % 15 ==0
+        "fizzbuzz"
+      when number % 3 == 0
+        "fizz"
+      when number % 5 == 0
+        "buzz"
+      else
+        number
     end
   end
 end
